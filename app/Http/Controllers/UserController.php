b31ab093e746f997c9e3f8879812e7ddb1334a4a
never97 <marco.silvestrin97@gmail.com>
2021-08-12 22:17:15 +0200

    Aggiunta opzioni crud

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,6 +5,7 @@ use App\Models\User;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
 
 class UserController extends Controller
 {
@@ -19,26 +20,45 @@ class UserController extends Controller
         $users = User::all();
         return view('users', ['listautenti' => $users]);
     }
+    public function edit(User $user)
+    {
+        return view('user.edit', compact('user'));
+    }
+    public function destroy(User $user)
+    {
+        $user->delete();
+
+        return redirect('/user');
+    }
 
     public function save(Request $request)
     {
         //validate
-        /*request()->validate([
+        $validator = Validator::make($request->all(), [
+
+        //request()->validate([
            'role' => 'required|max:10',
-           'email' => 'required|max:255',
-           'password'=> 'required|min:8|max:255',
-           'nome' => 'required|email|max:255',
-           'cognome' => 'required|max:255',
-        ]);*/
-        echo $request;
-        $user = new User;
-        $user->is_admin = ($request->input('role') === "admin");
-        $user->email = $request->input('email');
-        $user->password = Hash::make($request->input('password'));
-        $user->nome = $request->input('nome');
-        $user->cognome = $request->input('cognome');
-        $user->save();
-        return redirect()->route('user');
+           'email' => 'required|email|max:60|unique:users,email',
+           'password'=> 'required|min:8|max:50',
+           'nome' => 'required|min:2|max:50',
+           'cognome' => 'required|max:50',
+        ]);
+        if ($validator->fails()) {
+            return redirect()->back()
+                ->withErrors($validator)
+                ->withInput();
+        }else{
+            //echo $request;
+            $user = new User;
+            $user->is_admin = ($request->input('role') === "admin");
+            $user->email = $request->input('email');
+            $user->password = Hash::make($request->input('password'));
+            $user->nome = $request->input('nome');
+            $user->cognome = $request->input('cognome');
+            $user->save();
+            return redirect()->route('user');
+        }
+
     }
 
 
