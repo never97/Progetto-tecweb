c466f7da3eef1342b7962105c5db4108288e299d
never97 <marco.silvestrin97@gmail.com>
2021-08-31 11:49:48 +0200

    operazioni progetto

--- resources/views/operazioni.blade.php
+++ resources/views/operazioni.blade.php
@@ -6,40 +6,7 @@
 
             <div class="col-8">
                 <div class="row">
-{{--
-                            @if($progetti->isEmpty())
-                                <p>Non ci sono progetti presenti</p>
-                            @else
-                                <h1> Prima data </h1>
-                                <h1> Seconda data </h1>
 
-                                <table class="table">
-                                    <thead>
-                                    <tr>
-                                        <th>Cliente</th>
-                                    </tr>
-                                    </thead>
-                                    <tbody>
-                                    @foreach($progetti as $progetto)
-                                        <tr>
-                                            <th scope="row">{{ date('d/m/Y', strtotime($progetto->data_inizio_prevista)) }}</th>
-                                            <th scope="row">{{ date('d/m/Y', strtotime($progetto->data_fine_prevista)) }}</th>
-
-
-                                        </tr>
-                                    @endforeach
-
-                                    </tbody>
-                                </table>
-                            @endif
-                            <a href="{{ URL::action('App\Http\Controllers\ProgettoController@create') }}" class="btn btn-primary float-md-right mb-2">Aggiungi</a>
-
-                        </div>
-                    </div>
-                    --}}@if($ore->isEmpty())
-                        <p>Non ci sono schede presenti</p>
-                    @else
-                        <h1> Tutte le schede </h1>
                 </div>
                 <div class="row mb-2">
                     <form method="GET" action="{{action('App\Http\Controllers\SchedaOreController@filter')}}">
@@ -51,35 +18,84 @@
                                 <input type="date" class="form-control" name="data_fine" required value="{{$data_fine}}">
                             </div>
                             <input type="submit" class="btn btn-info" value="Cerca tra date"/>
-                            <input type="reset" class="btn btn-danger" value="Reset"/>
+                            <a href="operazioni" class="btn btn-info">indietro</a>
                         </div>
                     </form>
                 </div>
+
                 <div class="row">
                         <p>Sto filtrando da {{$data_inizio}} a {{$data_fine}}</p>
                 </div>
-                <table class="table">
-                    <thead>
-                    <tr>
-                        <th>Nome progetto</th>
-                        <th>Ore unitarie</th>
-                    </tr>
-                    </thead>
-                    <tbody>{{--                            <td class="somma">{{$ore}}</td>
---}}
-                    @foreach($ore as $ore1)
-                        <tr>
-                            <td class="somma">{{$ore1->nome}}</td>
-                            <td class="somma">{{$ore1->totale}}</td>
-                        </tr>
-                    @endforeach
-                    </tbody>
-                </table>
+                @if($ore->isEmpty())
+                        <p>Non ci sono schede presenti</p>
+                    @else
+
+                    <p>
+                        <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseProgetti" aria-expanded="false" aria-controls="collapseExample">
+                            Mostra le ore spese per ogni progetto
+                        </button>
+                    </p>
+
+                <div class="collapse" id="collapseProgetti">
+                    <div class="card card-body">
+                        <h1> Tutte le schede </h1>
+                        <table class="table">
+                            <thead>
+                            <tr>
+                                <th>Nome progetto</th>
+                                <th>Ore unitarie</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            @foreach($ore as $ore1)
+                                <tr>
+                                    <td class="somma">{{$ore1->nome}}</td>
+                                    <td class="somma">{{$ore1->totale}}</td>
+                                </tr>
+                            @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+                @endif
+                @if($clientiProg->isEmpty())
+                    <p>Non ci sono schede presenti</p>
+                @else
+
+                    <p>
+                        <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseClienti" aria-expanded="false" aria-controls="collapseExample">
+                            Mostra le ore spese per ogni cliente
+                        </button>
+                    </p>
+
+                    <div class="collapse" id="collapseClienti">
+                        <div class="card card-body">
+                            <h1> Tutte le schede </h1>
+                            <table class="table">
+                                <thead>
+                                <tr>
+                                    <th>Nome cliente</th>
+                                    <th>Ore unitarie</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                @foreach($clientiProg as $ore1)
+                                    <tr>
+                                        <td class="somma">{{$ore1->nome_referente}}</td>
+                                        <td class="somma">{{$ore1->totale}}</td>
+                                    </tr>
+                                @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
                 @endif
             </div>
         </div>
     </div>
     </div>
+
+
 @endsection
 
 
